402a78514a8a41f8959d46e2d73918fd8df7e0a1
ValeriaGael <valeriaguilherme.s@gmail.com>
2023-09-17 22:36:32 -0300

    alteração nome

--- Escola/src/Escola/Escola.java
+++ Escola/src/Escola/Escola.java
@@ -2,7 +2,7 @@ package Escola;
 public class Escola {
 	public static void main(String[] args) {
 		Aluno felipe = new Aluno();
-		felipe.nome="Felipe";
+		felipe.nome="Felipi";
 		felipe.idade = 8;
 		
 		System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
